db238e29886fd8da7c6d4b12c79846e1de075a2a
Edwin Encomienda <encomienda000@gmail.com>
2018-04-07 09:56:37 +0800

    updates

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Event Scheduler">
     <title>Event Scheduler</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
