a0f9e3cb1a92ada2eb0acfd19fed3b88822bd52b
Edwin Encomienda <encomienda000@gmail.com>
2018-04-19 12:22:29 +0800

    updates

--- database/migrations/2018_04_17_121149_alter_users_table_add_column_user_name.php
+++ database/migrations/2018_04_17_121149_alter_users_table_add_column_user_name.php
@@ -14,7 +14,7 @@ class AlterUsersTableAddColumnUserName extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->unique();
+            // $table->string('username')->unique();
         });
     }
 
